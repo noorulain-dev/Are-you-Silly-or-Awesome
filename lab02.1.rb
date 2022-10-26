12b924aaec0d1628dd69207814b1634433b27c46
Noor ul Ain Khurshid <80438950+noorulain1234@users.noreply.github.com>
2022-10-27 01:04:14 +0300

    Add files via upload

---
+++ lab02.1.rb
@@ -0,0 +1,21 @@
+def main
+
+ 
+ puts "Enter your name: "
+ name = gets.chomp
+ i = 1
+ 
+ if name == ('Noor' or 'Sim Kwan Hua')
+      print name + ' is an awesome name'
+	  else print name + ' is a '
+		     while ( i < 11 ) 
+			 print' silly'
+					  i = i+1
+				      end 
+				       print ' name '
+					  end
+					  
+					
+end
+
+main
